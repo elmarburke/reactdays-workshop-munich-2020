e1602cc86a24849a9d6db279f11d7d86d74e291f
Elmar Burke <hi@elmar.codes>
2020-09-02 14:02:56 +0200

    lists

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from "react";
 import Message from "./components/MessageView";
+import { Message as MessageType } from "./domain";
 
 const App = () => {
+  const messages: readonly MessageType[] = [
+    {
+      id: "1",
+      author: "Elmar",
+      message: "React is ...",
+      date: Date.now(),
+    },
+    {
+      id: "2",
+      author: "Christian",
+      message: "PHP is better",
+      date: Date.now(),
+    },
+  ];
+
   return (
     <>
-      <Message
-        message={{
-          id: "1",
-          author: "Elmar",
-          message: "React is ...",
-          date: Date.now(),
-        }}
-      />
-      <Message
-        message={{
-          id: "2",
-          author: "Christan",
-          message: "PHP is better",
-          date: Date.now(),
-        }}
-      />
+      {messages.map((message) => (
+        <Message key={message.id} message={message} />
+      ))}
     </>
   );
 };
