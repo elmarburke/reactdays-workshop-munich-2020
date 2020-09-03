8478ca0ffdc05ac62ff03c2ab9e5d03739a39614
Elmar Burke <hi@elmar.codes>
2020-09-03 11:54:35 +0200

    redux in hook with "broken" reducer

--- src/hooks/useMessages.ts
+++ src/hooks/useMessages.ts
@@ -1,23 +1,28 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { AppState } from "../reducers/appReducer";
 import { Message } from "../domain";
 
 const useMessages = () => {
-  const [messages, setMessage] = React.useState<readonly Message[]>([]);
+  const messages = useSelector<AppState, readonly Message[]>(
+    (state) => state.messageList
+  );
+  // const [messages, setMessage] = React.useState<readonly Message[]>([]);
 
-  React.useEffect(() => {
-    const controller = new AbortController();
+  // React.useEffect(() => {
+  //   const controller = new AbortController();
 
-    fetch("/messages.json", { signal: controller.signal })
-      .then((res) => res.json())
-      .then((data) => setMessage(data));
+  //   fetch("/messages.json", { signal: controller.signal })
+  //     .then((res) => res.json())
+  //     .then((data) => setMessage(data));
 
-    return () => {
-      controller.abort();
-    };
-  }, []);
+  //   return () => {
+  //     controller.abort();
+  //   };
+  // }, []);
 
   const addMessage = (message: Message): void => {
-    setMessage([...messages, message]);
+    // setMessage([...messages, message]);
   };
 
   return { messages, addMessage };
--- src/reducers/appReducer.ts
+++ src/reducers/appReducer.ts
@@ -1,7 +1,7 @@
-// import { Message } from "../domain";
+import { Message } from "../domain";
 
-interface AppState {
-  readonly messageList: readonly string[];
+export interface AppState {
+  readonly messageList: readonly Message[];
 }
 
 const initialState: AppState = {
@@ -12,7 +12,15 @@ function appReducer(state: AppState = initialState, action: any): AppState {
   if (action.type === "ADD_MESSAGE") {
     return {
       ...state,
-      messageList: [...state.messageList, action.text],
+      messageList: [
+        ...state.messageList,
+        {
+          id: `Message:${new Date().toJSON()}`,
+          author: "Elmar",
+          date: Date.now(),
+          message: action.text,
+        },
+      ],
     };
   }
 
