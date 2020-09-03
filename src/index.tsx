c551957718075b398391ad5587676c4cac7eb576
Elmar Burke <hi@elmar.codes>
2020-09-03 11:47:23 +0200

    provider store

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import App from "./App";
 import configureStore from "./configureStore";
 
 const store = configureStore();
 
-const element = <App />;
+const element = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 ReactDOM.render(element, document.getElementById("root"));
